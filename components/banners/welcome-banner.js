f11cc12984771deb3c775fab750289d18b518993
Branon Eusebio <branoneusebio@Branons-MacBook-Pro.local>
2021-05-17 12:27:33 -0700

    Added framer motion library

--- components/banners/welcome-banner.js
+++ components/banners/welcome-banner.js
@@ -1,9 +1,9 @@
 import styles from './welcome-banner.module.scss'
 
-const WelcomeBanner = () => {
+const WelcomeBanner = (props) => {
     return (
         <div className={styles.WelcomeBanner}>
-            <h5>Welcome to Charter Intranet!</h5>
+            <h5>{props.title}</h5>
             <div className={styles.right}>
                 <div className={styles.block}></div>
                 <div className={styles.block}></div>
--- components/cards/resource-card.js
+++ components/cards/resource-card.js
@@ -1,9 +1,26 @@
 import {GearsIcon} from 'unify-design-icons'
+import {motion} from 'framer-motion'
 import styles from './resource-card.module.scss'
 
 const ResourceCard = (props) => {
     return (
-        <div className={styles.ResourceCard}>
+        <motion.div className={styles.ResourceCard}
+            initial={{ rotate: 330, scale: 0, opacity: 0 }}
+            animate={{ 
+                rotate: 360, 
+                scale: 1, 
+                opacity: 1
+            }}
+            transition={{ 
+                type: 'spring',
+                duration: 0.15
+            }}
+            whileHover={{
+                translateY: '-2px',
+                boxShadow: '0px 5px 12px rgba(0, 0, 0, 0.22)',
+                duration: 0.15
+            }}
+        >
             <GearsIcon className={styles.icon} />
             <h6 className='detail-title'>{props.title}</h6>
             <p className={styles.description}>{props.description}</p>
@@ -11,7 +28,7 @@ const ResourceCard = (props) => {
             <a href={props.link} className={styles.button}>
                 Read more
             </a>
-        </div>
+        </motion.div>
     )
 }
 
